c5e1b015778e39d815840fd3341d071e05efe5e1
Akmalxo'ja Sharifxo'jayev <akmalxojasharifxojayev@gmail.com>
2024-04-24 14:19:00 +0500

    Navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,42 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+
+
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { NavbarComponent } from './companents/navbar/navbar.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    RegisterComponent,
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatButtonModule,
+    RouterModule
   ],
-  providers: [],
+  providers: [
+    provideAnimationsAsync()
+  
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
